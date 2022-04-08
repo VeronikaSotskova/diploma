dd9f2ec4ced306fa4825ff14971756622d012923
Veronika <verasos13@gmail.com>
2022-04-08 09:55:12 +0300

    СТрÐтраница поиска

--- front/src/views/TableTreeSearch.vue
+++ front/src/views/TableTreeSearch.vue
@@ -1,21 +1,69 @@
 <template lang="pug">
-p {{ $route.params.searchText }}
-p {{ tableHierarchy }}
+nav.navbar.navbar-light.bg-light
+  a(@click="goToMain" role="button").btn.btn-outline-secondary Go to Main
+  form.form-inline
+    input.form-control.mr-sm-2(
+      type="search"
+      placeholder="Search"
+      aria-label="Search"
+      list="items"
+      v-model="searchQuery"
+      @keyup="submitSearch"
+      @enter="goToSearchPage"
+    )
+    button.btn.btn-outline-success.my-2.my-sm-0(
+      type="submit"
+      @click="goToSearchPage"
+    ) Search
+    datalist#items
+      option(v-for="(table, index) in tables" :value="table" :key="index")
+tree(
+  :nodes="nodes"
+  :config="config"
+)
 </template>
 
 <script>
+import treeview from "vue3-treeview";
+import "vue3-treeview/dist/style.css";
 
 export default {
   name: "TableTreeSearch",
 
-  mounted() {
-    this.$store.dispatch("tables/getTableHierarchy", {name: this.$route.params.searchText});
+  components: {
+    tree: treeview,
+  },
+
+  data() {
+    return {
+      nodes: {
+      },
+      config: {roots: []},
+      searchQuery: ''
+    }
   },
 
   computed: {
-    tableHierarchy() {
-      return this.$store.getters["tables/tableHierarchy"]
+    tables() {
+      return this.$store.getters["tables/tables"]
+    }
+  },
+  mounted() {
+    this.$store.dispatch("tables/getTableHierarchy", {name: this.$route.params.searchText}).then((data) => {
+      this.nodes = data.nodes;
+      this.config = data.config;
+    });
+  },
+  methods: {
+    goToSearchPage() {
+      this.$router.push({path: `/search/${this.searchQuery}`});
     },
+    submitSearch() {
+      return this.$store.dispatch('tables/getTables', {q: this.searchQuery})
+    },
+    goToMain() {
+       this.$router.push({path: '/'});
+    }
   }
 }
 </script>
